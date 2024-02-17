5410465eb45976347599de72967566c580da8f53
Jonathan Schneider <jkschneider@gmail.com>
2024-02-16 22:14:45 -0500

    More

--- src/main/java/io/moderne/RecipeDot.java
+++ src/main/java/io/moderne/RecipeDot.java
@@ -13,17 +13,20 @@ import java.util.List;
 
 public class RecipeDot {
     public static void main(String[] args) throws IOException {
-//        String recipeId = "org.openrewrite.java.spring.boot3.UpgradeSpringBoot_3_2";
+        String recipeId = "org.openrewrite.java.spring.boot3.UpgradeSpringBoot_3_2";
 //        String recipeId = "org.openrewrite.staticanalysis.CommonStaticAnalysis";
-        String recipeId = "org.openrewrite.java.testing.junit5.JUnit4to5Migration";
+//        String recipeId = "org.openrewrite.java.testing.junit5.JUnit4to5Migration";
 
-        Recipe recipe = Environment.builder().scanRuntimeClasspath().build().activateRecipes(recipeId);
+        Recipe recipe = Environment.builder().scanRuntimeClasspath().build().activateRecipes(recipeId)
+                .getRecipeList().get(0);
 
         StringBuilder g = new StringBuilder();
         g.append("digraph G {\n");
-        g.append("  ratio=\"fill\";\n");
-        g.append("  size=\"12,5!\";\n");
-        writeNodes(recipe.getDescriptor(), g);
+//        g.append("  ratio=\"fill\";\n");
+//        g.append("  size=\"12,5!\";\n");
+        g.append("  layout=sfdp;\n");
+        g.append("  graph [ranksep=3, overlap=prism];\n");
+        writeNodes(recipe.getDescriptor(), g, 0, 0);
         writeEdges(recipe.getDescriptor(), g);
         g.append("}");
 
@@ -35,24 +38,24 @@ public class RecipeDot {
         Files.write(out.resolve(recipeId + ".dot"), g.toString().getBytes());
     }
 
-    private static void writeNodes(RecipeDescriptor descriptor, StringBuilder g) {
-        int dupCount = 0;
+    private static void writeNodes(RecipeDescriptor descriptor, StringBuilder g, int rank, int dupCount) {
+        int nextDupCount = 0;
         List<RecipeDescriptor> recipeList = descriptor.getRecipeList();
         for (int i = 0; i < recipeList.size(); i++) {
             RecipeDescriptor next = recipeList.get(i);
             if (i < recipeList.size() - 1 &&
                 next.getName().equals(recipeList.get(i + 1).getName()) &&
                 next.getRecipeList().isEmpty()) {
-                dupCount++;
+                nextDupCount++;
                 continue;
             }
-            writeNodes(next, g);
+            writeNodes(next, g, rank + 1, nextDupCount);
         }
         g.append("  ").append(nodeName(descriptor)).append(" [label=\"")
 //                .append(descriptor.getDisplayName())
                 .append(WordUtils.wrap(descriptor.getDisplayName(), 20)) //.replace("\n", "\\\n"))
                 .append(dupCount == 0 ? "" : " (x" + dupCount + ")")
-                .append("\"];\n");
+                .append("\",fillcolor=\"").append(rank == 0 ? "#99ffcc" : "#99aaff").append("\",style=\"filled\"];\n");
     }
 
     private static void writeEdges(RecipeDescriptor descriptor, StringBuilder g) {
@@ -67,7 +70,7 @@ public class RecipeDot {
             // could we stack cards when there are many of the same type of recipe but with
             // different options in the list?
             g.append("  ").append(nodeName(descriptor))
-                    .append(" -> ").append(nodeName(next)).append(";\n");
+                    .append(" -> ").append(nodeName(next)).append(" [color=\"#737a8440\"];\n");
             writeEdges(next, g);
         }
     }
